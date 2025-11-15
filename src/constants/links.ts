63029905ef01eed7eaca1e63e5416032988c3025
Olaleye Blessing <Olayinkablexxy@gmail.com>
2025-11-15 15:19:57 +0100

    Redirect to new website

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,26 +1,26 @@
 export const LINKS = {
-  contributorForm:
-    "https://docs.google.com/forms/d/e/1FAIpQLSfOWubPChHH14LpV4GwgXrrot0Smqd1rmypN4MEULdw7n1o4g/viewform",
-  dashboard: "https://dune.com/bread_cooperative/solidarity",
-  discord: "https://discord.com/invite/zmNqsHRHDa",
-  docs: "https://docs.bread.coop",
-  docsBreadToken: "https://docs.bread.coop/bread-token/",
-  docsManifesto: "https://docs.bread.coop/manifesto",
-  docsHowToBecomeAMemberProject:
-    "https://docs.bread.coop/how-to-become-a-member-project",
-  docsVotingPower: "https://docs.bread.coop/voting-power",
-  farcaster: "https://farcaster.xyz/~/channel/cryptoleft",
-  giveth: "https://giveth.io/project/breadchain-cooperative",
-  github: "http://github.com/BreadchainCoop",
-  linkedin: "https://www.linkedin.com/company/bread-cooperative//",
-  newsletter: "http://paragraph.com/@breadcoop",
-  openCollective: "https://opencollective.com/bread-cooperative",
-  postCapitalistIdea: "https://form.typeform.com/to/opwqWG8j",
-  projectApplicationForm: "https://forms.gle/DeTETFxCxZbKRCzS7",
-  solidarityFund: "https://app.breadchain.xyz",
-  sourdoughSystems: "https://www.sourdough.systems/",
-  twitter: "https://x.com/breadcoop",
-  youtube: "https://www.youtube.com/@BreadCooperative/",
+	contributorForm:
+		"https://docs.google.com/forms/d/e/1FAIpQLSfOWubPChHH14LpV4GwgXrrot0Smqd1rmypN4MEULdw7n1o4g/viewform",
+	dashboard: "https://dune.com/bread_cooperative/solidarity",
+	discord: "https://discord.com/invite/zmNqsHRHDa",
+	docs: "https://docs.bread.coop",
+	docsBreadToken: "https://docs.bread.coop/bread-token/",
+	docsManifesto: "https://docs.bread.coop/manifesto",
+	docsHowToBecomeAMemberProject:
+		"https://docs.bread.coop/how-to-become-a-member-project",
+	docsVotingPower: "https://docs.bread.coop/voting-power",
+	farcaster: "https://farcaster.xyz/~/channel/cryptoleft",
+	giveth: "https://giveth.io/project/breadchain-cooperative",
+	github: "http://github.com/BreadchainCoop",
+	linkedin: "https://www.linkedin.com/company/bread-cooperative//",
+	newsletter: "http://paragraph.com/@breadcoop",
+	openCollective: "https://opencollective.com/bread-cooperative",
+	postCapitalistIdea: "https://form.typeform.com/to/opwqWG8j",
+	projectApplicationForm: "https://forms.gle/DeTETFxCxZbKRCzS7",
+	solidarityFund: "https://fund.bread.coop",
+	sourdoughSystems: "https://www.sourdough.systems/",
+	twitter: "https://x.com/breadcoop",
+	youtube: "https://www.youtube.com/@BreadCooperative/",
 } as const;
 
 export type LinkKey = keyof typeof LINKS;
